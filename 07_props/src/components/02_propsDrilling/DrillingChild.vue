e7f7b6a1d1cde1e728e3637aaacd57243cb13a75
JaeWon Lee <jlee38266@gmail.com>
2024-03-26 15:55:00 +0900

    vue.js props 활용 예제
    props란 부모 컴포넌트에서 자식 컴포넌트로 데이터를 넘길 수 있는 방법입니다.  하지만 물려 받는 자식이 많을 경우, props drilling 문제가 발생할 수 있으며 이를 해결하기 위해 provide를 활용할 수 있습니다. 다만 provide는 자식 컴포넌트에서 수정이 불가능 하도록 부모 컴포넌트에서 readonly 옵션을 설정할 수 있습니다. 다만 provide의 단점은 누가 부모 컴포넌트를 사용하는지 관리하기 어렵다는 단점이 있습니다.

---
+++ 07_props/src/components/02_propsDrilling/DrillingChild.vue
@@ -0,0 +1,11 @@
+<template>
+    <h2>Child : {{ injectCount }}</h2>
+    <GrandChild/>
+</template>
+
+<script setup>
+    import { inject } from 'vue';
+    import GrandChild from './DrillingGrandChild.vue';
+
+    const injectCount = inject('count');
+</script>
